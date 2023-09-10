61d89f09b0ae4fd070d35ea0f9e1b2a4a9e61888
Tia4130 <Tiaakiki12@gmail.com>
2023-09-10 12:16:36 +0300

    Setting up the chatBot Kit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import { Chatbot } from 'react-chatbot-kit';
+
+//COMPONENTS FOR THE CHATBOT
+import config from './config';
+import MessageParser from './MessageParser';
+import ActionProvider from './ActionProvider';
 
 function App() {
   return (
     <div className="App">
-      hello
+      <Chatbot config={config}
+        messageParser={MessageParser}
+        actionProvider={ActionProvider} />
     </div>
   );
 }
